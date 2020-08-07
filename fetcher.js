072527085d015f02dcb038a565efd020e7d366d1
Tenyson Kwon <tenysonkwon@gmail.com>
2020-08-07 16:41:26 +0000

    used request lib to write server response into file

--- fetcher.js
+++ fetcher.js
@@ -2,14 +2,23 @@ const request = require('request');
 const fs = require('fs');
 
 const args = process.argv.slice(2);
-//first arg, http://www.example.edu/
+//first arg, http://www.example.edu/  
+//https://www.google.com/fdsafsafsa.html
 //second arg should be  ./index.html  
 //print message: Downloaded and saved 1235 bytes to ./index.html
 
 request(args[0], (error, response, body) => {
-  fs.writeFile(args[1], body, (error) => {
-    const pageSize = fs.statSync(args[1]).size;
-    console.log(`Downloaded and saved ${pageSize} to ${args[1]}`);
+  
+  fs.writeFile(args[1], body, (err) => {
+    if (err) {    //local filepath invalid handler
+      console.log("invalid path / file DNE", err);
+    } else if (error || response.statusCode !== 200) { //URL error handler
+      console.log('Error occurred, see below: \n', error, '\n', response.statusCode);
+      return;
+    } else {
+      const pageSize = fs.statSync(args[1]).size;
+      console.log(`Downloaded and saved ${pageSize} to ${args[1]}`);
+    }
   });
 });
 
